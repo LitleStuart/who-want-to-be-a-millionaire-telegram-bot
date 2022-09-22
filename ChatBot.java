47d954794270f4caade577c67371c82fbcd811ce
N1kowinter <N1kowinter@HOME-PC>
2022-09-22 12:40:57 +0500

    Structure of Chat Bot with comments

--- ChatBot.java
+++ ChatBot.java
@@ -1,3 +1,8 @@
 public class ChatBot {
+    public static String generateAnswer(int userID, String userMessage) // rework dialogState to enum
+    {
+        String answer = "";
+        return answer;
+    }
 
 }
