1e5864c84b8c23b0c3c071fe51b235b59de1c315
Ivan Mukhorin <mukhorinvanga@gmail.com>
2022-09-25 21:30:00 +0500

    update OOP

---
+++ TgBotApi.java
@@ -0,0 +1,33 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class TgBotApi extends TelegramLongPollingBot implements IBotApi {
+    private IOnUpdate updateHandler;
+
+    @Override
+    public void registerOnUpdate(IOnUpdate onUpdate) {
+        this.updateHandler = onUpdate;
+    }
+
+    @Override
+    public void sendAnswer(String text) {
+        System.out.println(text);
+    }
+
+    @Override
+    public void onUpdateReceived(org.telegram.telegrambots.meta.api.objects.Update update) {
+        String message = update.getMessage().getText();
+        long chatId = update.getMessage().getChatId();
+        updateHandler.onUpdate(new Update(chatId, new Message(message)));
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+}
